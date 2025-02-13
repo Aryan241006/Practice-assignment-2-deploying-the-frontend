88de4dcbbef1ad46b2272480824623b510b21fef
Aryan241006 <aryananand410@gmail.com>
2025-02-13 09:45:42 +0530

    new changes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Aryan Anand</h1>
+      <p>Email: aryan.anand.s66@kalvium.community</p>
+      <p>Phone: 1234567890</p>
+    </div>
+  );
+}
+
+export default App;
+
